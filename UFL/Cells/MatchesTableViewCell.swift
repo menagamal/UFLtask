6018f5928f161b0ec25856c54e8623829aea2975
MenaGamal <menagamal203@yahoo.com>
2018-06-18 22:33:27 +0200

    firstCommit

---
+++ UFL/Cells/MatchesTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MatchesTableViewCell.swift
+//  UFL
+//
+//  Created by Mena Gamal on 6/18/18.
+//  Copyright © 2018 Mena Gamal. All rights reserved.
+//
+
+import UIKit
+
+class MatchesTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
---
+++ UFL/Match.swift
@@ -0,0 +1,9 @@
+//
+//  Match.swift
+//  UFL
+//
+//  Created by Mena Gamal on 6/17/18.
+//  Copyright © 2018 Mena Gamal. All rights reserved.
+//
+
+import Foundation
